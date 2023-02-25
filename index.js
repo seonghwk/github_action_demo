bef39e02711abc67c0f1e7341480e9d3d9bb5dc8
Seonghwan Kim <seonghwan1031@gmail.com>
2023-02-25 23:52:36 +0900

    Fix api_call_axios.yml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const getMenu = async () => {
   try {
-    const response = await axios.get("'http://54.180.126.55:8080/api/menus");
+    const response = await axios.get("http://54.180.126.55:8080/api/menus");
     console.log(response.data);
   } catch (error) {
     console.log("ERROR");
